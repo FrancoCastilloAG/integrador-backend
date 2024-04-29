b303fd3d21ef4817088352deaeb2d8fac4cf050b
franco.castillo2811@gmail.com <franco.castillo2811@gmail.com>
2024-04-29 00:01:46 -0400

    fix: prisma schema definition

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserInput } from './dto/create-user.input';
 import { PrismaService } from '../prisma.service';
-import { User } from '@prisma/client';
+import { Prisma, PrismaClient, User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(public prisma: PrismaService){}
   
-  async create(createUserInput: CreateUserInput): Promise<User> {
+  async create(createUserInput: Prisma.UserCreateInput): Promise<User> {
     return this.prisma.user.create({ data: createUserInput });
   }
 }
